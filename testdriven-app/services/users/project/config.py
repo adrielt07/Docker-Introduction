159e6f4c35866793f8db38c280de578804f1ac47
EC2 Default User <ec2-user@ip-172-31-10-234.us-west-1.compute.internal>
2019-01-27 23:03:07 +0000

    get environment variable set for SQLalchemy

--- testdriven-app/services/users/project/config.py
+++ testdriven-app/services/users/project/config.py
@@ -1,17 +1,21 @@
 # services/users/project/config.py
 
+from os import getenv
+
 class BaseConfig:
     '''Base Configuration'''
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATION = False
 
 class DevelopmentConfig(BaseConfig):
     '''Development Configuration'''
-    pass
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL', "")
 
 class TestingConfig(BaseConfig):
     '''Testing Configuration'''
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_TEST_URL', "")
 
 class ProductionConfig(BaseConfig):
     '''Production Configuration'''
-    pass
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL', "")
