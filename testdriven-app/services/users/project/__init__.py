901e54c008593346194268312f1c219ea055e1c3
EC2 Default User <ec2-user@ip-172-31-10-234.us-west-1.compute.internal>
2019-01-27 22:10:47 +0000

    use environment variable set in the container and added error check

--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -1,12 +1,17 @@
 # services/users/project/__init__.py
 
+import os, sys
 from flask import Flask, jsonify
 
 # Instantiate the app
 app = Flask(__name__)
 
 # Set Configuration
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# Error check
+print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
@@ -15,4 +20,3 @@ def ping_pong():
         'status': 'success',
         'message': 'pong'
     })
-
