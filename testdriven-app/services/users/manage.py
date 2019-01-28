168047f3a54f5949a34a78bc6e1e827b8d3579a5
EC2 Default User <ec2-user@ip-172-31-10-234.us-west-1.compute.internal>
2019-01-28 22:16:36 +0000

    change the imported packages

--- testdriven-app/services/users/manage.py
+++ testdriven-app/services/users/manage.py
@@ -1,7 +1,17 @@
 from flask.cli import FlaskGroup
-from project import app
+from project.app import app, db
 
 cli = FlaskGroup(app)
+
+@cli.command()
+def recreatedb():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command()
+def hello():
+    print("Hello world")
 
 if __name__ == '__main__':
     cli()
