1b9141467dd7a6c153b950f710dc4c8dd9c38d23
EC2 Default User <ec2-user@ip-172-31-10-234.us-west-1.compute.internal>
2019-01-28 22:10:35 +0000

    move functions and to a separate files

--- testdriven-app/services/users/project/config.py
+++ testdriven-app/services/users/project/config.py
@@ -1,21 +1,21 @@
 # services/users/project/config.py
 
-from os import getenv
+import os
 
 class BaseConfig:
     '''Base Configuration'''
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     '''Development Configuration'''
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL', "")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     '''Testing Configuration'''
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_TEST_URL', "")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     '''Production Configuration'''
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL', "")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
--- testdriven-app/services/users/project/users.py
+++ testdriven-app/services/users/project/users.py
@@ -1,11 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from __init__ import app
+# services/users/project/users.py
 
-# Instantiate the db
-db = SQLAlchemy(app)
+from project.app import app, db
 
 # User model
 class User(db.Model):
+
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), nullable=False)
