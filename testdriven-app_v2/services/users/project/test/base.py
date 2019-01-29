a628d4cb4b78105a2f0148ac091ecdc48c7a3241
adrielt07 <adriel_tolentino@outlook.com>
2019-01-29 23:09:28 +0000

    modified import

--- testdriven-app_v2/services/users/project/test/base.py
+++ testdriven-app_v2/services/users/project/test/base.py
@@ -1,7 +1,10 @@
 # services/users/project/test/base.py
 
 from flask_testing import TestCase
-from project.app import db, app
+from project import create_app
+from project import db
+
+app = create_app()
 
 class BaseTestCase(TestCase):
     def create_app(self):
