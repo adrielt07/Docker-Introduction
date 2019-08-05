5af386bf0f4268ff25b0618b6489ef1fa7f6202a
atolentino <atolentino@box.com>
2019-08-05 16:20:57 -0700

    Revert "removed route endpoint and modified import"
    This reverts commit 186a58317118065467f256ae0e047ebcef1018da.

--- testdriven-app_v2/services/users/project/__init__.py
+++ testdriven-app_v2/services/users/project/__init__.py
@@ -1,34 +1,14 @@
 # services/users/project/__init__.py
 
 import os, sys
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from project.app import app
+from project.users import User
+from flask import jsonify
 
-print("Starting app...")
-
-# Instantiate the db
-db = SQLAlchemy()
-
-def create_app(script_info=None):
-    # Instantiate the app
-    app = Flask(__name__)
-
-    # Set Configuration
-    app_settings = os.getenv('APP_SETTINGS')
-    app.config.from_object(app_settings)
-
-    db.init_app(app)
-
-    # register blueprints
-    from project.api.users import users_blueprint
-    app.register_blueprint(users_blueprint)
-
-    # Error check
-    print(app.config, file=sys.stderr)
-
-    # shell context for clask cli
-    @app.shell_context_processor
-    def ctx():
-        return {'app': app, 'db': db}
-
-    return app
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    '''Checks the status of API'''
+    return jsonify({
+        'status': 'success',
+        'message': 'pong'
+    })
